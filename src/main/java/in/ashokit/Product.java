d22d5de98d5f4276a923097367967bb7c90712cf
Milan-Git3420 <milankumarnayakj3420@gmail.com>
2023-07-24 23:46:41 +0530

    this ismy first commit

---
+++ src/main/java/in/ashokit/Product.java
@@ -0,0 +1,19 @@
+package in.ashokit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	
+	
+	private Integer id; 
+	private String name;
+	private Double price;
+	
+	
+
+}
